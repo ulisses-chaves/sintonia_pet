6629331d7c05f35ca915ea606a873056592232ff
= <ramicesmoises@gmail.com>
2019-07-16 14:35:00 -0300

    Commit 03

---
+++ src/main/java/com/models/Email.java
@@ -0,0 +1,66 @@
+package com.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "email")
+public class Email  {
+
+	
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy="increment")
+	private long id;
+	private String email;
+	private String nome;
+	private String mensagem;
+	
+	
+	public Email()
+	{
+		this.email ="";
+		this.mensagem = "";
+		this.nome = "";
+	}
+	
+	public Email(String email, String nome, String mensagem)
+	{
+		this.email = email;
+		this.nome = nome;
+		this.mensagem = mensagem;
+		
+	}
+			
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getnome() {
+		return nome;
+	}
+	public void setnome(String nome) {
+		this.nome = nome;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+}
